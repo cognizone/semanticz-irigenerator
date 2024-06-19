2586b2d1cf27d98aae89cc5cebda0653bfc4c685
psiotwo <petr.kremen@cogni.zone>
2024-06-19 16:31:40 +0200

    refactor: cleanup, removing Spring from tests, JDK 11 compatibility

--- src/main/java/zone/cogni/asquare/cube/urigenerator/json/UriGeneratorRoot.java
+++ src/main/java/zone/cogni/asquare/cube/urigenerator/json/UriGeneratorRoot.java
@@ -2,38 +2,30 @@ package zone.cogni.asquare.cube.urigenerator.json;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.commons.lang3.StringUtils;
-import org.springframework.core.io.InputStreamSource;
-import org.springframework.core.io.Resource;
 import zone.cogni.asquare.cube.json5.Json5Light;
 
 import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 public class UriGeneratorRoot {
 
-  public static UriGeneratorRoot load(InputStreamSource resource) {
+  public static UriGeneratorRoot load(URL resource) {
     try {
       ObjectMapper mapper = Json5Light.getJson5Mapper();
-      return mapper.readValue(resource.getInputStream(), UriGeneratorRoot.class);
-    }
-    catch (IOException e) {
-      String extra = getPath(resource) != null ? " Path " + getPath(resource) : "";
+      return mapper.readValue(resource.openStream(), UriGeneratorRoot.class);
+    } catch (IOException e) {
+      String extra;
+      try {
+        extra = " URL " + resource.toURI();
+      } catch (URISyntaxException ex) {
+        throw new RuntimeException(ex);
+      }
       throw new RuntimeException("Unable to load uri generator configuration." + extra, e);
     }
-  }
-
-  private static String getPath(InputStreamSource resource) {
-    try {
-      if (resource instanceof Resource r) {
-        return r.getFile().getPath();
-      }
-    }
-    catch (IOException e) {
-      return null;
-    }
-    return null;
   }
 
   private Map<String, String> prefixes;
@@ -60,11 +52,11 @@ public class UriGeneratorRoot {
 
   public String getPrefixQuery() {
     return prefixes.entrySet()
-                   .stream()
-                   .map(e -> "PREFIX "
-                             + StringUtils.rightPad(e.getKey() + ":", 8) + " <" + e.getValue() + ">\n")
-                   .collect(Collectors.joining())
-           + "\n";
+            .stream()
+            .map(e -> "PREFIX "
+                    + StringUtils.rightPad(e.getKey() + ":", 8) + " <" + e.getValue() + ">\n")
+            .collect(Collectors.joining())
+            + "\n";
   }
 
 }
