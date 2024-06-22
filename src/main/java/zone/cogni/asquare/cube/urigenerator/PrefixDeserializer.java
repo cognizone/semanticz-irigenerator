dc9b53ea9719366897cb4ef90e679e842fde90d6
psiotwo <petr.kremen@cogni.zone>
2024-06-22 09:16:00 +0200

    feat: custom deserializer for prefixes in JSON5

---
+++ src/main/java/zone/cogni/asquare/cube/urigenerator/PrefixDeserializer.java
@@ -0,0 +1,38 @@
+package zone.cogni.asquare.cube.urigenerator;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import zone.cogni.asquare.cube.urigenerator.json.Prefix;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * Deserializes the map of prefixes into Java POJOs.
+ *
+ * TODO: note that this is here only to ensure compliance of the new data model with the original JSON5 serialization.
+ */
+public class PrefixDeserializer extends JsonDeserializer<List<Prefix>> {
+
+  @Override
+  public List<Prefix> deserialize(JsonParser p, DeserializationContext ctxt)
+          throws IOException {
+    final List<Prefix> keyValueList = new ArrayList<>();
+    final ObjectMapper mapper = (ObjectMapper) p.getCodec();
+    final JsonNode rootNode = mapper.readTree(p);
+
+    final Iterator<String> fieldNames = rootNode.fieldNames();
+    while (fieldNames.hasNext()) {
+      final String key = fieldNames.next();
+      final String value = rootNode.get(key).asText();
+      keyValueList.add(new Prefix().setKey(key).setValue(value));
+    }
+
+    return keyValueList;
+  }
+}
--- src/main/java/zone/cogni/asquare/cube/urigenerator/json/Prefix.java
+++ src/main/java/zone/cogni/asquare/cube/urigenerator/json/Prefix.java
@@ -4,11 +4,13 @@ import cz.cvut.kbss.jopa.model.annotations.Id;
 import cz.cvut.kbss.jopa.model.annotations.OWLClass;
 import cz.cvut.kbss.jopa.model.annotations.OWLDataProperty;
 import lombok.Data;
+import lombok.experimental.Accessors;
 import zone.cogni.asquare.cube.urigenerator.Vocabulary;
 
 import java.net.URI;
 
 @Data
+@Accessors(chain = true)
 @OWLClass(iri = Vocabulary.C_PREFIX)
 public class Prefix {
 
