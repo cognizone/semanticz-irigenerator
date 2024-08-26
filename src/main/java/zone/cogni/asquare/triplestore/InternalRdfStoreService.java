a59d0bc487da6295dc609a02f5b4842a21deed05
psiotwo <petr.kremen@cogni.zone>
2024-08-26 16:51:19 +0200

    fix: InternalRdfStoreService cleanup

--- src/main/java/zone/cogni/asquare/triplestore/InternalRdfStoreService.java
+++ src/main/java/zone/cogni/asquare/triplestore/InternalRdfStoreService.java
@@ -19,19 +19,17 @@
 
 package zone.cogni.asquare.triplestore;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.jena.query.*;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.shared.Lock;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import zone.cogni.sem.jena.template.JenaResultSetHandler;
 
 import java.io.Closeable;
 import java.util.function.Supplier;
 
+@Slf4j
 public class InternalRdfStoreService implements RdfStoreServiceAPI, Closeable {
-
-  private static final Logger log = LoggerFactory.getLogger(InternalRdfStoreService.class);
 
   private final Model model;
 
@@ -41,6 +39,7 @@ public class InternalRdfStoreService implements RdfStoreServiceAPI, Closeable {
 
   @Override
   public boolean executeAskQuery(Query query, QuerySolutionMap bindings) {
+    trace(query);
     return executeInLock(() -> {
       try (QueryExecution queryExecution = QueryExecution.create()
               .query(query)
@@ -49,7 +48,7 @@ public class InternalRdfStoreService implements RdfStoreServiceAPI, Closeable {
               .build()) {
         return queryExecution.execAsk();
       } catch (RuntimeException e) {
-        log.error("Query failed: {}", query);
+        error(query);
         throw e;
       }
     });
@@ -57,12 +56,9 @@ public class InternalRdfStoreService implements RdfStoreServiceAPI, Closeable {
 
   public <R> R executeSelectQuery(String query, JenaResultSetHandler<R> resultSetHandler) {
     Query parsedQuery = QueryFactory.create(query, Syntax.syntaxARQ);
+    trace(parsedQuery);
     QuerySolutionMap bindings = new QuerySolutionMap();
     return executeInLock(() -> {
-      if (log.isTraceEnabled()) log.trace("Select {} \n{}",
-              bindings,
-              parsedQuery);
-
       try (QueryExecution queryExecution = QueryExecution.create()
               .query(parsedQuery)
               .model(model)
@@ -71,7 +67,7 @@ public class InternalRdfStoreService implements RdfStoreServiceAPI, Closeable {
         ResultSet resultSet = queryExecution.execSelect();
         return resultSetHandler.handle(resultSet);
       } catch (RuntimeException e) {
-        log.error("Query failed: {}", parsedQuery);
+        error(parsedQuery);
         throw e;
       }
     });
@@ -79,20 +75,27 @@ public class InternalRdfStoreService implements RdfStoreServiceAPI, Closeable {
 
   public Model executeConstructQuery(String query) {
     Query parsedQuery = QueryFactory.create(query, Syntax.syntaxARQ);
+    trace(parsedQuery);
     return executeInLock(() -> {
-
       try (QueryExecution queryExecution = QueryExecution.create()
               .query(parsedQuery)
               .model(model)
               .build()
       ) {
-        if (log.isTraceEnabled()) log.trace("Running construct query: \n{}", parsedQuery);
         return queryExecution.execConstruct();
       } catch (RuntimeException e) {
-        log.error("Query failed: {}", parsedQuery);
+        error(parsedQuery);
         throw e;
       }
     });
+  }
+
+  private void trace(final Query query) {
+    log.trace("Running query: {}", query);
+  }
+
+  private void error(final Query query) {
+    log.error("Query failed: {}", query);
   }
 
   @Override
